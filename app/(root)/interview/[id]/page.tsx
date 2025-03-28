83cf2d2537cc32166e8f855f053e9f5f94786c8e
AryanRathore04 <aryan4meu@gmail.com>
2025-03-29 01:13:47 +0530

    show feedback

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -10,7 +10,7 @@ import React from "react";
 
 const page = async ({ params }: RouteParams) => {
   const { id } = await params;
-  const user = await getCurrentUser()
+  const user = await getCurrentUser();
 
   const interview = await getInterviewbyId(id);
 
@@ -19,18 +19,32 @@ const page = async ({ params }: RouteParams) => {
     <>
       <div className="flex flex-row gap-4 justify-between">
         <div className="flex flex-row gap-4 items-center max-sm:flex-col">
-            <div className="flex flex-row gap-4 items-center">
-                <Image src={getRandomInterviewCover()} alt= "cover-image" width={40} height={40} className="rounded-full object-cover size-[40px]"/>
-                <h3 className="capitalize">{interview.role} Interview</h3>
-            </div>
+          <div className="flex flex-row gap-4 items-center">
+            <Image
+              src={getRandomInterviewCover()}
+              alt="cover-image"
+              width={40}
+              height={40}
+              className="rounded-full object-cover size-[40px]"
+            />
+            <h3 className="capitalize">{interview.role} Interview</h3>
+          </div>
 
-            <DisplayTechIcons techStack={interview.techstack} />
+          <DisplayTechIcons techStack={interview.techstack} />
         </div>
 
-        <p className="bg-dark-200 px-4 py-2 rounded-lg h-fit capitalize">{interview.type}</p>
+        <p className="bg-dark-200 px-4 py-2 rounded-lg h-fit capitalize">
+          {interview.type}
+        </p>
       </div>
 
-      <Agent userName={user?.name} type={user?.id} interviewId={id} type="interview" questions={interview.questions} />
+      <Agent
+        userName={user?.name || ''}
+        userId={user?.id}
+        interviewId={id}
+        type="interview"
+        questions={interview.questions}
+      />
     </>
   );
 };
